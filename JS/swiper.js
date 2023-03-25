5f7f42241495cf62057c43cb1bab095a2c03d2e8
Pintelek <andreypintel@mail.ru>
2023-03-26 00:38:51 +0300

    add Swiper

---
+++ JS/swiper.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    createElements: true,
+    effect: 'fade',
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+})
