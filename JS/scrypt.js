3ff695b2ae323b48a790318557ba42cfd28b129f
Pintelek <andreypintel@mail.ru>
2023-03-28 23:55:30 +0300

    add Form Window

--- JS/scrypt.js
+++ JS/scrypt.js
@@ -3,26 +3,26 @@ const navLinks = document.querySelectorAll('.nav-how-link');
 let activeNum;
 activeitem();
 // console.log(navLinks)
-function activeitem(){
-navLinks.forEach((item, key) => {
-  if(item.classList.contains('active')){
-    activeNum = key;
-    colSteps[key].classList.add('active')
-  }
-  item.addEventListener('click', changeActive)
-})
+function activeitem() {
+  navLinks.forEach((item, key) => {
+    if (item.classList.contains('active')) {
+      activeNum = key;
+      colSteps[key].classList.add('active')
+    }
+    item.addEventListener('click', changeActive)
+  })
 }
-function deliteClass(elem){
-  elem.forEach(item =>{
-    if(item.classList.contains('active')){
+function deliteClass(elem) {
+  elem.forEach(item => {
+    if (item.classList.contains('active')) {
       item.classList.remove('active');
     }
   })
- 
+
 }
 
-function changeActive(){
-  if(!this.classList.contains('active')){
+function changeActive() {
+  if (!this.classList.contains('active')) {
     deliteClass(navLinks)
     deliteClass(colSteps)
     this.classList.add('active')
@@ -36,15 +36,15 @@ function changeActive(){
 const sliders = document.querySelector('.hero-slider');
 const cilcle = document.querySelectorAll('.ellipse');
 
-cilcle.forEach( (item, key) => item.addEventListener('click', ()=> {
+cilcle.forEach((item, key) => item.addEventListener('click', () => {
   nextImg(key)
 }))
 
-function nextImg(key){
+function nextImg(key) {
   deliteClass(cilcle);
 
   cilcle[key].classList.add('active');
-  sliders.style.transform = `translateX(${key * -100/3}%)`;
+  sliders.style.transform = `translateX(${key * -100 / 3}%)`;
 }
 
 // ================= FAQ =============
@@ -62,16 +62,59 @@ function nextImg(key){
 // }))
 // =============== BURGER ===========
 
-document.querySelector('.burger').addEventListener('click', ()=>{
+document.querySelector('.burger').addEventListener('click', () => {
   document.querySelector('.burger-up').classList.toggle('active');
   document.querySelector('.burger-header').classList.toggle('active');
 })
 
-window.addEventListener('scroll', ()=>{
-  if(window.scrollY > 200){
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
     document.querySelector('.burger-up').classList.remove('active');
-  document.querySelector('.burger-header').classList.remove('active');
-  console.log(window.scrollY);
+    document.querySelector('.burger-header').classList.remove('active');
   }
-  
+
+})
+
+// FORM===============
+
+const btnForm = document.querySelector('.btn-footer-form');
+
+
+btnForm.addEventListener('click', (event) => {
+  const form = document.querySelector('.footer-form');
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  let name = formData.get('name');
+  let Email = formData.get('Email');
+  let message = formData.get('message')
+
+  if (name && Email && message) {
+    const upWindow = document.createElement('div');
+    upWindow.classList.add('form-window');
+
+    const windowTitle = document.createElement('h3');
+    windowTitle.classList.add('window-title');
+    windowTitle.append('Спасибо за заявку.');
+
+    const emailWindow = document.createElement('p');
+    emailWindow.classList.add('email-window');
+    emailWindow.append(`Уважаемый/ая , ${name}, ваша заявка принята к рассмотрению. Ожидайте сообщения на ${Email}`)
+
+    upWindow.append(emailWindow, windowTitle);
+
+    document.querySelector('.footer').append(upWindow);
+    setTimeout(() => {
+      console.log('dkskskdakdadadasdasdad');
+      upWindow.classList.add('active')
+    }, 300);
+    setTimeout(() => {
+      window.addEventListener('click', () => {
+        console.log(upWindow);
+        upWindow.remove();
+      })
+    }, 1500)
+  }
+
 })
